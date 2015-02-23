eb4613e7ee1b2f7e0ddeff5d5b0aab7b3bb02cc2
Kiernan809 <lee.kiernan@gmail.com>
2015-02-23 22:43:03 +0000

    Init from home.  Same theory different project from start

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,97 @@
+var d3 = require('d3')
+,   nv = require('nvd3')
+,   nv = window.nv // What?
+;; // End vars.
+
+// require('nvd3/nv.d3.css');
 require('../css/main.scss');
-var $ = require('jquery'),
-    d3 = require('d3'),
-    nvd3 = require('nv.d3'),
-    data = require('./data');
 
-var width = 640;
-var height = 480;
-var root = d3.select('#chart').append('svg')
-    .attr({
-      'width': width,
-      'height': height,
-    });
-
-// Render the title.
-var titleHeight = 50;
-root.append('text')
-    .attr({
-      'class': 'title',
-      'x': width / 2,
-      'y': titleHeight / 2,
-    })
-    .text('Skull-splitting power!');
-
-// Simulate 500 rolls of the axe.
-var rollDie = function(numSides) {
-  return 1 + Math.floor(Math.random() * numSides);
+var rates = {
+    // threshold: %.rate,
+    previous: {
+        0: 0.00,
+        125000: 0.01,
+        250000: 0.03,
+        500000: 0.04,
+        1000000: 0.05,
+        2000000: 0.07
+    },
+    current: {
+        0: 0.00,
+        125000: 0.02,
+        250000: 0.05,
+        925000: 0.10,
+        1500000: 0.12
+    }
 };
 
-var MAX_ROLL = 4 + 4 + 6 + 6;
-var rollHisto = d3.range(MAX_ROLL + 1).map(function() { return 0; });
-for (var i = 0; i < 500; i++) {
-  var rolled = rollDie(4) + rollDie(4) + rollDie(6) + rollDie(6);
-  rollHisto[rolled]++;
-}
+window.d3Data = [{
+    0: 0.00,
+    125000: 0.01,
+    250000: 0.03,
+    500000: 0.04,
+    1000000: 0.05,
+    2000000: 0.07
+}, {
+    0: 0.00,
+    125000: 0.02,
+    250000: 0.05,
+    925000: 0.10,
+    1500000: 0.12
+}];
 
-// Render our axis.
-var yAxisWidth = 50;
-var xAxisHeight = 50;
 
-var xScale = d3.scale.linear()
-    .domain([0, rollHisto.length])
-    .range([yAxisWidth, width]);
-var yScale = d3.scale.linear()
-    .domain([0, d3.max(rollHisto) * 1.2])
-    .range([height - xAxisHeight, titleHeight]);
+// var root = d3.select('#chart').append('svg')
+//     .attr('width', '100%')
+//     .attr('height', 400)
+//     .style('border', '1px solid black');
 
-var xAxis = d3.svg.axis().scale(xScale);
-root.append('g')
-    .attr({
-      'class': 'x axis',
-      'transform': 'translate(0,' + (height - xAxisHeight) + ')',
-    })
-    .call(xAxis);
+// root.selectAll('rect')
+//     .data([5, 25, 80]).enter()
+//   .append('rect')
+//     .attr('x', Object)
+//     .attr('y', Object)
+//     .attr('width', 15)
+//     .attr('height', 10)
+//     .attr('fill', '#c63')
+//     .attr('stroke', 'black');
 
-var yAxis = d3.svg.axis().scale(yScale).orient('left');
-root.append('g')
-    .attr({
-      'class': 'y axis',
-      'transform': 'translate(' + yAxisWidth + ',0)',
-    })
-    .call(yAxis);
+console.log("nv: ", nv);
+d3.json('/js/cumulativeLineData.json', function(data) {
+    console.log("nv: ", nv);
+    nv.addGraph( function() {
+        var chart = nv.models.cumulativeLineChart()
+            .x(function(d) {
+                return d[0]
+            })
+            .y(function(d) {
+                return d[1] / 100
+            }) //adjusting, 100% is 1.00, not 100 as it is in the data
+            .color(d3.scale.category10().range())
+            .useInteractiveGuideline(true);
 
-// Render the dice bars.
-root.selectAll('rect.bar')
-    .data(rollHisto).enter()
-  .append('rect')
-    .attr({
-      'class': 'bar',
-      'x': function(d, i) { return xScale(i - 0.5); },
-      'width': xScale(1) - xScale(0),
-      'y': yScale,
-      'height': function(d) { return yScale(0) - yScale(d); },
+        chart.xAxis
+            .tickValues([1078030800000, 1122782400000, 1167541200000, 1251691200000])
+            .tickFormat(function(d) {
+                return d3.time.format('%x')(new Date(d))
+            });
+
+        chart.yAxis
+            .tickFormat(d3.format(',.1%'));
+
+        d3.select('#chart svg')
+            .datum(data)
+            .call(chart);
+
+        //TODO: Figure out a good way to do this automatically
+        nv.utils.windowResize(chart.update);
+
+        return chart;
     });
+});
+
+
+setTimeout( function() {
+    var nv = require('nvd3')
+    console.log("NV: ", nv);
+}, 10000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+,   path = require('path')
+,   webpack = require('webpack');
 
 var config = {
   devtool: 'eval',
@@ -7,40 +8,52 @@ var config = {
   entry: './src/js/main.js',
 
   output: {
-    path: 'build/',
-    filename: '[name].js',
+    path: './build',
+    // path: process.env.NODE_ENV === 'production' ? './dast' : './build',
+    filename: 'build.js',
     chunkFilename: '[id].chunk.js',
-    publicPath: '/build/'
+    publicPath: '/__compiled/'
   },
 
   module: {
     loaders: [{
-      test: /\.json$/,
-      loader: 'json-loader'
-    }, {
-      test: /\.js$/,
-      loader: 'jsx-loader?harmony'
-    }, {
-      test: /\.scss$/,
-      loader: [
-        'style!css!sass?outputStyle=expanded',
-        '&includePaths[]=', path.resolve(__dirname, './bower_components'),
-        '&includePaths[]=', path.resolve(__dirname, './node_modules')
-      ].join('')
-    }]
+        test: /\.json$/,
+        loader: 'json-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'jsx-loader?harmony'
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader', 'css-loader!sass-loader?outputStyle=expanded' +
+          '&includePaths[]='+ (path.resolve(__dirname, './bower_components')) +
+          '&includePaths[]='+ (path.resolve(__dirname, './node_modules'))
+        )
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
   },
 
   resolve: {
-    root: [path.join(__dirname, "bower_components")]
+      root: [ path.join(__dirname, 'bower_components') ],
+      alias: {
+        // 'React' : path.join(__dirname, 'bower_components', 'react' )
+      }
   },
 
   plugins: [
-    new webpack.ResolverPlugin(
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
-    ),
     new webpack.optimize.CommonsChunkPlugin('shared.js'),
+
+    new ExtractTextPlugin('[name].css', {
+      allChunks: true
+    }),
+
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+    )
   ]
 };
-
 
 module.exports = config;
