5894b963a472f8720b62f9b2df263423efebc4ce
Kiernan809 <lee.kiernan@gmail.com>
2015-02-26 16:37:17 +0000

    Working redraw and intersection

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,120 @@
 var d3 = require('d3')
+,   $ = require('jquery')
+,   __ = require('lodash')
 ,   nv = require('nvd3')
 ,   nv = window.nv // What?
 ,   taxCalc = require('./calctax.js')
-,   __ = require('lodash')
+,   chart
+,   data
 ;; // End vars.
 
 require('../css/main.scss');
 require('../../node_modules/nvd3/nv.d3.css');
 
-
-d3.json('/js/taxData.json', function(data) {
+function redraw() {
     nv.addGraph( function() {
-        var chart = nv.models.lineChart()
-            .x( function(d) {
-                return d[0]
-            })
-            .y( function(d) {
-                return d[1]
-            }) //adjusting, 100% is 1.00, not 100 as it is in the data
-            .color( d3.scale.category10().range() )
-            .useInteractiveGuideline(true);
+        chart = nv.models.lineChart()
+        .x( function(d) { return d[0] })
+        .y( function(d) { return d[1] })
+        .color( d3.scale.category10().range() )
+        .useInteractiveGuideline(true);
 
         chart.xAxis
-            .tickValues([300000, 600000, 900000, 1500000, 2000000])
-            .tickFormat( function(d) {
-                var format = d3.format(',');
-                return '£' + format(d);
-            });
+        .tickFormat( function(d) {
+            var format = d3.format(',');
+            return '£' + format(d);
+        });
 
         chart.yAxis
-            .tickFormat( function(d) {
-                var format = d3.format(',');
-                return '£' + format(d);
-            });
+        .tickFormat( function(d) {
+            var format = d3.format(',');
+            return '£' + format(d);
+        });
 
         d3.select('#chart svg')
-            .datum(data)
-            .call(chart);
+        .datum(data)
+        .call(chart);
 
-        //TODO: Figure out a good way to do this automatically
         nv.utils.windowResize(chart.update);
 
-        return chart;
-    });
-});
+// var circle = d3.select('#chart svg').append("circle")
+//     .attr("opacity", 0)
+//     .attr({
+//     r: 6,
+//     fill: 'darkred'
 
-
-// console.log( "345k 2014 SDLT: ", taxCalc.getTax(2014, 345000) );
-// console.log( "345k 2015 SDLT: ", taxCalc.getTax(2015, 345000) );
-
-
-// // getInput
-// var stdIterator = 500;
-// var getInput = parseInt('1300000');
-
-// // nearest 5k...
-// var nearest = stdIterator * Math.round( getInput / stdIterator )
-
-// var display = new Array(2600);
-// display = __.map( display, function(n, i) {
-//     var x = stdIterator * (i + 1);
-
-//     // If number > 75k, input in the middle of graph.
-//     return [ (nearest - x >= 0 ? nearest - x : 0), nearest + x ];
-// })
-
-// display.push(getInput);
-// display = __.uniq( __.flattenDeep( display ) ).sort(function(a,b) { return a - b });
-
-// __.map( display, function(n, i) {
-//     return [n, parseInt(taxCalc.getTax(2014, n).toFixed(2)) ]
 // });
+
+    return chart;
+});
+}
+
+d3.json( '/js/taxData.json', function(e, json) {
+    data = json;
+    redraw();
+} );
+
+function inputToData(input, nValues, iterator) {
+    if( iterator === undefined ) { iterator = 500; }
+    if( nValues === undefined ) { nValues = 300; }
+
+    input = parseInt(input);
+
+    // nearest ((iterator))...
+    var nearest = iterator * Math.round( input / iterator )
+
+    // Create new array of x values.
+    var display = new Array( (nValues/2) );
+    display = __.map( display, function(n, i) {
+        var x = iterator * (i + 1);
+
+        return [ (nearest - x >= 0 ? nearest - x : 0), nearest + x ];
+    })
+
+    display.push(input); // add our input value to graph.
+    // Remove duplicate values (0), sort numerically.
+    display = __.uniq( __.flattenDeep( display ) ).sort(function(a,b) { return a - b });
+
+    // For each value, create x and y values to plot.
+    var data = [{
+        "key": "2014 ",
+        "values": __.map( display, function(n, i) { return [n, parseInt(taxCalc.getTax(2014, n).toFixed(2)) ] })
+    }, {
+        "key": "2015 ",
+        "values": __.map( display, function(n, i) { return [n, parseInt(taxCalc.getTax(2015, n).toFixed(2)) ] })
+    }];
+
+    return {
+        inputIndex : __.indexOf( display, input ),
+        data: data
+    }
+}
+
+$(function() {
+    $('form').on( 'submit', function(e) {
+        e.preventDefault();
+
+        var value = $('input').val();
+
+        inputValueData = inputToData( value )
+        data = inputValueData.data;
+        redraw();
+
+        var verticalLine = d3.select('#chart svg .nv-linesWrap').append('line')
+        .attr({
+            'x1': 0,
+            'x2': 0,
+            'y1': 0,
+            'y2': 500,
+            'stroke' : 'red',
+            'class' : 'lineV'
+        });
+
+        var position = data[0].values.length / inputValueData.inputIndex;
+        position = d3.select('rect').attr('width') / position;
+        verticalLine.attr({ 'transform': 'translate(770.9302325581394,0)' });
+
+
+        return false;
+    })
+});
